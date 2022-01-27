f20ae6b0180e2b3a321b19922399b7a21b44eca1
Kame NG <kamenascimento18@gmail.com>
2022-01-27 10:19:18 -0300

    Salvando alteração na atividade da uc8

---
+++ ExercicioPeca.js
@@ -0,0 +1,23 @@
+// iniciando o exercício da UC8.
+let PesoPeca = 100
+if(PesoPeca > 100) {
+    console.log("Peso maior que 100g, não podemos cadastrar")} 
+else{
+    console.log("peso menor que 100g, podemos cadastrar")
+}
+// Verificar número de peças cadastradas.
+let NumeroPecas = 10
+if(NumeroPecas >= 10) {
+    console.log("Número menor ou igual que 10, podemos cadastrar")}
+else{
+    console.log("Número maior que 10, não podemos cadastrar")
+}
+
+// Verificar o comprimento do nome da peça. 
+let comprimentoPeca = 'disco de freio'
+console.log("Comprimento do número da peça é: ", comprimentoPeca.length)
+if(comprimentoPeca.length >3) {
+    console.log("Caracteres maior que 3, podemos cadastrar.")
+} else{
+    Console.log("Carateres menores que 3, não podemos cadastrar")
+}
